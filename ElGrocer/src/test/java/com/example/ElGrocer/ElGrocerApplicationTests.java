128ed42a22a23775455f2cc31180d9d6cf93694a
mohankumar <mohankumar2112003@gmail.com>
2025-08-19 18:44:37 +0530

    first commit

---
+++ ElGrocer/src/test/java/com/example/ElGrocer/ElGrocerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ElGrocer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElGrocerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
